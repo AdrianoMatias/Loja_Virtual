a459ed704a77208b203ab7e9a137118dbbafc63f
Adriano-Matias <adriano-matias-souza@hotmail.com>
2020-07-09 11:32:27 -0300

    Consumindo produtos da API

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,88 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import { formatPrice } from '../../Util/format';
+import api from '../../services/api';
 
 import { ProductList } from './styles';
 
-export default function Home() {
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
+
+  async componentDidMount() {
+    const response = await api.get('products');
+
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }))
+
+    this.setState({ products: data });
+  }
+
+ render() {
+  const { products } = this.state;
+
   return (
     <ProductList>
-      <li>
-        <img src="https://static.netshoes.com.br/produtos/tenis-adidas-easy-vulc-20-masculino/72/NQQ-0133-172/NQQ-0133-172_zoom1.jpg?ts=1579544093&ims=326x"
-        alt="itens" />
-        <strong>Tênis muito legal</strong>
-        <span>R$ 129,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>ADIOCIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img src="https://static.netshoes.com.br/produtos/tenis-adidas-easy-vulc-20-masculino/72/NQQ-0133-172/NQQ-0133-172_zoom1.jpg?ts=1579544093&ims=326x"
-        alt="itens" />
-        <strong>Tênis muito legal</strong>
-        <span>R$ 129,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>ADIOCIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img src="https://static.netshoes.com.br/produtos/tenis-adidas-easy-vulc-20-masculino/72/NQQ-0133-172/NQQ-0133-172_zoom1.jpg?ts=1579544093&ims=326x"
-        alt="itens" />
-        <strong>Tênis muito legal</strong>
-        <span>R$ 129,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>ADIOCIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img src="https://static.netshoes.com.br/produtos/tenis-adidas-easy-vulc-20-masculino/72/NQQ-0133-172/NQQ-0133-172_zoom1.jpg?ts=1579544093&ims=326x"
-        alt="itens" />
-        <strong>Tênis muito legal</strong>
-        <span>R$ 129,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>ADIOCIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img src="https://static.netshoes.com.br/produtos/tenis-adidas-easy-vulc-20-masculino/72/NQQ-0133-172/NQQ-0133-172_zoom1.jpg?ts=1579544093&ims=326x"
-        alt="itens" />
-        <strong>Tênis muito legal</strong>
-        <span>R$ 129,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>ADIOCIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img src="https://static.netshoes.com.br/produtos/tenis-adidas-easy-vulc-20-masculino/72/NQQ-0133-172/NQQ-0133-172_zoom1.jpg?ts=1579544093&ims=326x"
-        alt="itens" />
-        <strong>Tênis muito legal</strong>
-        <span>R$ 129,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>ADIOCIONAR AO CARRINHO</span>
-        </button>
-      </li>
+      { products.map(product => (
+        <li key={product.id}>
+          <img
+            src={product.image}
+            alt={product.title}
+          />
+          <strong>{product.title}</strong>
+          <span>{product.priceFormatted}</span>
+          <button type="button">
+            <div>
+              <MdAddShoppingCart size={16} color="#FFF" /> 3
+            </div>
+            <span>ADIOCIONAR AO CARRINHO</span>
+          </button>
+        </li>
+      ))}
     </ProductList>
   );
+ }
 }
