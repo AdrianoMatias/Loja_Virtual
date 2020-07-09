203a32983630b2815d220d66b37d4b70b22d0e31
Adriano-Matias <adriano-matias-souza@hotmail.com>
2020-07-09 09:40:54 -0300

    Estilizando page Cart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,59 @@
 import React from 'react';
+import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  return <h1>Carrinho</h1>;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <td>
+            <img src="https://static.netshoes.com.br/produtos/tenis-adidas-easy-vulc-20-masculino/72/NQQ-0133-172/NQQ-0133-172_zoom1.jpg?ts=1579544093&ims=326x"
+             alt=""
+             />
+          </td>
+          <td>
+            <strong>Tênis muito massa</strong>
+            <span>129,90</span>
+          </td>
+          <td>
+            <button type="button">
+              <MdRemoveCircleOutline size={20} color="#7159c1" />
+            </button>
+            <input type="number" readOnly value={2} />
+            <button type="button">
+              <MdAddCircleOutline size={20} color="#7159c1" />
+            </button>
+          </td>
+          <td>
+            <strong>R$258,80</strong>
+          </td>
+          <td>
+            <button type="button">
+              <MdDelete size={20} color="#7159c1" />
+            </button>
+          </td>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finalizar pedido</button>
+
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$1920,28</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
---
+++ src/pages/Cart/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  padding: 30px;
+  background: #FFF;
+  border-radius: 4px;
+`;
+
+export const ProductTable = styled.table `
+
+`;
+
+export const Total = styled.div `
+
+`;
